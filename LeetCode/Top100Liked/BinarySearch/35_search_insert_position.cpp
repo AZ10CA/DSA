00274a403698e1aa37329570b85b9174e4405b81
Alireza Zahedi <alirezazahedi@Alirezas-Mac-Studio.local>
2024-03-07 11:55:29 -0500

    solve LeetCode search_insert_position

---
+++ LeetCode/Top100Liked/BinarySearch/35_search_insert_position.cpp
@@ -0,0 +1,25 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int searchInsert(vector<int> &nums, int target) {
+        // Time: O(logn), Space: O(1)
+        int left = 0, right = nums.size() - 1;
+        while (left <= right) {
+            auto middle = left + (right - left) / 2;
+            auto num = nums[middle];
+
+            if (target == num)
+                return middle;
+
+            if (target > num)
+                left = middle + 1;
+            else
+                right = middle - 1;
+        }
+
+        return left;
+    }
+};
