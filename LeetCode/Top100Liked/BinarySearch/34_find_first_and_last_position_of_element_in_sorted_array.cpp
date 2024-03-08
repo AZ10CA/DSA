7b22d94d7f7d208a82ba6dc6da18085de0de49ba
Alireza Zahedi <alirezazahedi@Alirezas-Mac-Studio.local>
2024-03-08 09:06:39 -0500

    solve LeetCode 34_find_first_and_last_position_of_element_in_sorted_array

---
+++ LeetCode/Top100Liked/BinarySearch/34_find_first_and_last_position_of_element_in_sorted_array.cpp
@@ -0,0 +1,35 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> searchRange(vector<int> &nums, int target) {
+        // Time: O(logn), Space: O(1)
+        auto right = find(nums, target, false);
+        auto left = find(nums, target, true);
+
+        return {left, right};
+    }
+
+    int find(const vector<int> &nums, int target, bool leftmost) {
+        int left = 0, right = nums.size() - 1;
+        int result = -1;
+        while (left <= right) {
+            auto middle = left + (right - left) / 2;
+            if (target == nums[middle]) {
+                if (leftmost)
+                    right = middle - 1;
+                else
+                    left = middle + 1;
+                result = middle;
+            } else if (target > nums[middle]) {
+                left = middle + 1;
+            } else {
+                right = middle - 1;
+            }
+        }
+
+        return result;
+    }
+};
