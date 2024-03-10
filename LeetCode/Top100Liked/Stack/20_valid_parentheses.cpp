2e475caa8b5973f4ac7b6ae91acbaf47c09b606a
Alireza Zahedi <alirezazahedi@Alirezas-Mac-Studio.local>
2024-03-10 14:53:28 -0400

    solve LeetCode 20_valid_parentheses

---
+++ LeetCode/Top100Liked/Stack/20_valid_parentheses.cpp
@@ -0,0 +1,32 @@
+#include <stack>
+#include <unordered_set>
+
+using namespace std;
+
+class Solution {
+public:
+    unordered_set<char> opening = {'(', '[', '{'};
+    unordered_set<char> closing = {')', '}', ']'};
+
+    bool isValid(string s) {
+        // Time: O(n), Space: O(n)
+        stack<char> st;
+        for (auto ch: s) {
+            if (opening.find(ch) != opening.end()) {
+                st.push(ch);
+            } else if (closing.find(ch) != closing.end()) {
+                if (st.empty())
+                    return false;
+                if (!isMatching(st.top(), ch))
+                    return false;
+                st.pop();
+            }
+        }
+        return st.empty();
+    }
+
+    bool isMatching(char first, char second) {
+        return first == '(' && second == ')' || first == '{' && second == '}' || first == '[' && second == ']';
+    }
+
+};
