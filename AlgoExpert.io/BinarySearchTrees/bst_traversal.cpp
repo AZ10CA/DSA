8e0d508d8fed712d47f9203e46222caf5223dfe9
AZ10CA <az.zahedi.alireza@gmail.com>
2024-03-22 13:45:48 -0400

    solve AlgoExpert bst_traversal

---
+++ AlgoExpert.io/BinarySearchTrees/bst_traversal.cpp
@@ -0,0 +1,39 @@
+#include <vector>
+using namespace std;
+
+class BST {
+public:
+    int value;
+    BST* left;
+    BST* right;
+
+    BST(int val);
+};
+
+
+void inOrderTraverse(BST* tree, vector<int>& array) {
+    // Time: O(n), Space: O(d)
+    if(not tree)
+        return;
+    inOrderTraverse(tree->left, array);
+    array.push_back(tree->value);
+    inOrderTraverse(tree->right, array);
+}
+
+void preOrderTraverse(BST* tree, vector<int>& array) {
+    // Time: O(n), Space: O(d)
+    if(not tree)
+        return;
+    array.push_back(tree->value);
+    preOrderTraverse(tree->left, array);
+    preOrderTraverse(tree->right, array);
+}
+
+void postOrderTraverse(BST* tree, vector<int>& array) {
+    // Time: O(n), Space: O(d)
+    if(not tree)
+        return;
+    postOrderTraverse(tree->left, array);
+    postOrderTraverse(tree->right, array);
+    array.push_back(tree->value);
+}
