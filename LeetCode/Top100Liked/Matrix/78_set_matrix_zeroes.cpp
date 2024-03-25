f1135ebbd320bfe1ad0bcbb040aa69e75f91b6f9
AZ10CA <az.zahedi.alireza@gmail.com>
2024-03-25 09:20:14 -0400

    solve LeetCode 78_set_matrix_zeroes

---
+++ LeetCode/Top100Liked/Matrix/78_set_matrix_zeroes.cpp
@@ -0,0 +1,24 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    void setZeroes(vector<vector<int>> &matrix) {
+        // Time: O(mn), Space: O(m + n)
+        unordered_set<int> rows;
+        unordered_set<int> cols;
+
+        for (int i = 0; i < matrix.size(); i++)
+            for (int j = 0; j < matrix[0].size(); j++)
+                if (matrix[i][j] == 0) {
+                    rows.insert(i);
+                    cols.insert(j);
+                }
+
+        for (int i = 0; i < matrix.size(); i++)
+            for (int j = 0; j < matrix[0].size(); j++)
+                if (rows.find(i) != rows.end() || cols.find(j) != cols.end())
+                    matrix[i][j] = 0;
+    }
+};
