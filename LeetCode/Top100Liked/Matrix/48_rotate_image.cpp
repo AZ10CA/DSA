0b656c3b6f561865c8c3f4e48517a0460a2caab1
AZ10CA <az.zahedi.alireza@gmail.com>
2024-03-25 09:14:01 -0400

    solve LeetCode 48_rotate_image

---
+++ LeetCode/Top100Liked/Matrix/48_rotate_image.cpp
@@ -0,0 +1,16 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    void rotate(vector<vector<int>> &matrix) {
+        // Time: O(n^2), Space: O(1)
+        for (int i = 0; i < matrix.size(); i++)
+            for (int j = i; j < matrix.size(); j++)
+                std::swap(matrix[i][j], matrix[j][i]);
+
+        for (int i = 0; i < matrix.size(); i++)
+            std::reverse(matrix[i].begin(), matrix[i].end());
+    }
+};
