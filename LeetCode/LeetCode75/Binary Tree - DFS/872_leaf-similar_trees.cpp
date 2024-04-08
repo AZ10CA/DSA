ccb3c718147874e5b960224a558982e4a551504a
AZ10CA <az.zahedi.alireza@gmail.com>
2024-04-08 09:45:34 -0400

    solve LeetCode 872_leaf-similar_trees

---
+++ LeetCode/LeetCode75/Binary Tree - DFS/872_leaf-similar_trees.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool leafSimilar(TreeNode* root1, TreeNode* root2) {
+        vector<int> leaves1, leaves2;
+        get_leaves(root1, leaves1);
+        get_leaves(root2, leaves2);
+
+        return leaves1 == leaves2;
+    }
+
+    void get_leaves(TreeNode* root, vector<int>& result){
+        if(!root)
+            return;
+
+        if(!root->left && !root->right)
+            result.push_back(root->val);
+
+        get_leaves(root->left, result);
+        get_leaves(root->right, result);
+    }
+};
