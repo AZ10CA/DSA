451d8c01ec37d3470b0da2c8942a030f09efa12d
AZ10CA <az.zahedi.alireza@gmail.com>
2024-05-31 16:52:45 -0400

    solve LeetCode 144_binary_tree_preorder_traversal

---
+++ LeetCode/Random/Binary Tree/144_binary_tree_preorder_traversal.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        // Time: O(n), Space: O(n)
+        vector<int> result;
+        traverse(root, result);
+        return result;
+    }
+
+    void traverse(TreeNode* root, vector<int>& result){
+        if(not root)
+            return;
+
+        result.push_back(root->val);
+        traverse(root->left, result);
+        traverse(root->right, result);
+    }
+};
