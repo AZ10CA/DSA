23b3c63a4ea6e73dfdf5047b0d5ca6fb434de0c1
AZ10CA <az.zahedi.alireza@gmail.com>
2024-09-05 16:59:26 -0400

    solve Grind75 insert_interval

---
+++ Grind75/insert_interval.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        vector<vector<int>> result;
+        int idx = 0;
+
+        while(idx < intervals.size() && intervals[idx][1] < newInterval[0])
+            result.push_back(intervals[idx++]);
+
+        while(idx < intervals.size() && is_overlapping(intervals[idx], newInterval)){
+            newInterval = merge(intervals[idx], newInterval);
+            idx++;
+        }
+
+        result.push_back(newInterval);
+
+        while(idx < intervals.size())
+            result.push_back(intervals[idx++]);
+
+        return result;
+    }
+
+    bool is_overlapping(const vector<int>& first, const vector<int>& second){
+        return first[0] >= second[0] && first[0] <= second[1] || second[0] >= first[0] && second[0] <= first[1];
+    }
+
+    vector<int> merge(const vector<int>& first, const vector<int>& second){
+        return {std::min(first[0], second[0]), std::max(first[1], second[1])};
+    }
+};
