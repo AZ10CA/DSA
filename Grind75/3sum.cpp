33d8d4a812ced8386dc29db580271ade3afe42c1
AZ10CA <az.zahedi.alireza@gmail.com>
2024-09-05 19:06:51 -0400

    solve Grind75 3sum

---
+++ Grind75/3sum.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        // Time: O(n), Space: O(n)
+        vector<vector<int>> result;
+        std::sort(nums.begin(), nums.end());
+
+        for(int i = 0; i < nums.size() - 2; i++){
+            auto selected = nums[i];
+            if(i > 0 && nums[i] == nums[i - 1])
+                continue;
+            int left = i + 1, right = nums.size() - 1;
+
+            while(left < right){
+                auto sum = nums[left] + nums[right];
+                if(sum + selected == 0){
+                    vector<int> triplet = {selected, nums[left], nums[right]};
+                    result.push_back(triplet);
+
+                    while(left < right && nums[left] == nums[left + 1])
+                        left++;
+
+                    while(left < right && nums[right] == nums[right - 1])
+                        right--;
+
+                    left++;
+                    right--;
+                } else if(sum + selected < 0)
+                    left++;
+                else right--;
+            }
+        }
+
+        return result;
+    }
+};
