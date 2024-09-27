8254228111ebf336ae18885eba7fff6b953adbe5
AZ10CA <az.zahedi.alireza@gmail.com>
2024-09-27 13:08:10 -0400

    solve Grind75 basic_calculator_ii

---
+++ Grind75/basic_calculator_ii.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int calculate(string s) {
+        // Time: O(n), Space: O(n)
+        char last_sign = '+';
+        long long current_number = 0;
+
+        stack<long long> operands;
+        s.push_back('+');
+
+        for(int i = 0; i < s.size(); i++){
+            if(s[i] == ' ')
+                continue;
+
+            auto current_char = s[i];
+            if(isdigit(current_char))
+                current_number = current_number * 10 + current_char - '0';
+            else {
+                if(last_sign == '+' || last_sign == '-')
+                    operands.push((last_sign == '-' ? -1 : 1) * current_number);
+                else {
+                    auto top = operands.top();
+                    operands.pop();
+                    auto result = last_sign == '*' ? top * current_number : top / current_number;
+                    operands.push(result);
+                }
+
+                last_sign = current_char;
+                current_number = 0;
+            }
+        }
+
+        long long result = 0;
+        while(!operands.empty()){
+            result += operands.top();
+            operands.pop();
+        }
+
+        return result;
+    }
+};
