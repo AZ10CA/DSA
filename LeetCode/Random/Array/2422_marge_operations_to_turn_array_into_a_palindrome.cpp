a278681c33b7f269875b037948e52b5f616aab77
AZ10CA <az.zahedi.alireza@gmail.com>
2024-11-22 11:52:00 -0500

    solve LeetCode 2422_marge_operations_to_turn_array_into_a_palindrome

---
+++ LeetCode/Random/Array/2422_marge_operations_to_turn_array_into_a_palindrome.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int minimumOperations(vector<int>& nums) {
+        // Time: O(n), Space: O(1)
+        std::ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
+
+        int l_idx = 0, r_idx = nums.size() - 1, operations = 0;
+
+        while(l_idx < r_idx){
+            if(nums[l_idx] == nums[r_idx]){
+                l_idx++;
+                r_idx--;
+                continue;
+            }
+
+            auto& smaller = nums[l_idx] < nums[r_idx] ? nums[l_idx] : nums[r_idx];
+            auto& larger = smaller == nums[l_idx] ? nums[r_idx] : nums[l_idx];
+
+            larger -= smaller;
+            smaller = 0;
+
+
+            if(nums[l_idx] < nums[r_idx]){
+                l_idx++;
+                operations++;
+                continue;
+            } else {
+                r_idx--;
+                operations++;
+                continue;
+            }
+        }
+
+        return operations;
+    }
+
+};
