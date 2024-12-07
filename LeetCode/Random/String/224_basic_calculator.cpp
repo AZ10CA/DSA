de73a0b835566d25393ced164afbac502436ce67
AZ10CA <az.zahedi.alireza@gmail.com>
2024-12-07 14:02:58 -0500

    solve LeetCode 224_basic_calculator

---
+++ LeetCode/Random/String/224_basic_calculator.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    Solution(){
+        cin.tie(nullptr);
+        cout.tie(nullptr);
+        std::ios::sync_with_stdio(false);
+    }
+
+    int calculate(string s) {
+        int idx = 0;
+        return evaluate(idx, "(" + s + ")");
+    }
+
+    int evaluate(int& from, const string& str){
+        // Time: O(n), Space: O(n)
+        // starts evaluating startin from "from" idx and then returns the position of the character after the closing parenthesis
+        int sum = 0;
+        char oper = '+';
+        int iterator = from + 1;
+        int current_num = 0;
+
+        while(iterator < str.size()){
+            if(str[iterator] == ' '){
+                iterator++;
+                continue;
+            }
+
+            if(std::isdigit(str[iterator])){
+                current_num = current_num * 10ll + str[iterator] - '0';
+                iterator++;
+            } else if(str[iterator] == '('){
+                auto nested_sum = evaluate(iterator, str);
+                sum = process(sum, oper, nested_sum);
+            } else if(str[iterator] == ')') {
+                from = iterator + 1;
+                return process(sum, oper, current_num);
+            } else {
+                // is an operator
+                sum = process(sum, oper, current_num);
+                current_num = 0;
+                oper = str[iterator];
+                iterator++;
+            }
+        }
+
+        from = iterator;
+        return process(sum, oper, current_num);
+    }
+
+    int process(int left, char oper, int right){
+        return oper == '-' ? left - right : left + right;
+    }
+};
