4fa3a80a56ffdd5c65bf5d4ae2dfac44d673b688
AZ10CA <az.zahedi.alireza@gmail.com>
2025-04-29 12:00:27 -0400

    fix the commit bug

--- LeetCode/Random/String/161_one_edit_distance.cpp
+++ LeetCode/Random/String/161_one_edit_distance.cpp
@@ -1,3 +1,29 @@
-//
-// Created by Alireza Zahedi on 2025-04-29.
-//
+class Solution {
+public:
+    bool isOneEditDistance(string s, string t) {
+        // Time: O(n), Space: O(n)
+        int cost = 0;
+        auto distance = hamming_distance(0, 0, cost, s, t);
+        return distance == 1;
+    }
+
+    bool hamming_distance(int s_idx, int t_idx, int cost, const string& s, const string& t){
+        if(s_idx == s.size())
+            return (cost + t.size() - t_idx) == 1;
+        if(t_idx == t.size())
+            return (cost + s.size() - s_idx) == 1;
+
+        if(cost > 1)
+            return false;
+
+        if(s[s_idx] == t[t_idx])
+            return hamming_distance(s_idx + 1, t_idx + 1, cost, s, t);
+
+        // characters are not the same
+        auto add = hamming_distance(s_idx, t_idx + 1, cost + 1, s, t);
+        auto deletion = hamming_distance(s_idx + 1, t_idx, cost + 1, s, t);
+        auto replace = hamming_distance(s_idx + 1, t_idx + 1, cost + 1, s, t);
+
+        return add || deletion || replace;
+    }
+};
--- LeetCode/Random/String/266_palindrome_permutation.cpp
+++ LeetCode/Random/String/266_palindrome_permutation.cpp
@@ -1,3 +1,23 @@
-//
-// Created by Alireza Zahedi on 2025-04-29.
-//
+class Solution {
+public:
+    bool canPermutePalindrome(string s) {
+        // Time: O(n), Space: O(1)
+        array<int, 26> ft;
+        for(auto letter: s)
+            ft[letter - 'a']++;
+
+
+        bool found_odd = false;
+        for(auto each: ft){
+            if(each % 2 == 0)
+                continue;
+            else{
+                if(found_odd == true)
+                    return false;
+                found_odd = true;
+            }
+        }
+
+        return true;
+    }
+};
